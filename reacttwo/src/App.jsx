544d56af83a209d0a9e94507377a3326971da2af
DeveloperAbhishekSNaik <abhisheksatishnaik@gmail.com>
2023-09-05 07:03:09 -0700

    vite commit

---
+++ reacttwo/src/App.jsx
@@ -0,0 +1,16 @@
+import Abhi from './abhi'
+
+function App() {
+  
+
+  return (
+    <>
+    <Abhi />
+    <p>My aspiration is to become full stack developer and also learn every technology
+    regarding Web related and also in future mobile related.</p>
+    </>
+  
+  )
+}
+
+export default App;
